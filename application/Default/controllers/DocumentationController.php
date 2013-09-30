fea6d711c3b207b6e52fa96953e1bc5f8cd79c78
josh <josh.ribakoff@gmail.com>
2013-09-30 16:41:09 -0400

    remove vendor, composer.lock, .idea and add to .gitnore along with downloads folder which contains .tar.gz files

--- application/Default/controllers/DocumentationController.php
+++ application/Default/controllers/DocumentationController.php
@@ -2,17 +2,35 @@
 class DocumentationController extends Zend_Controller_Action
 {
 
-    function preDispatch()
-    {
-        $this->render('navigation','navigation',true);
-    }
-
     function indexAction()
     {
         $url = str_replace('/documentation/index/','',$this->_request->getRequestUri());
-        var_dump(file_get_contents(BASE_PATH.'/html/vf-documentation/toc.htm'));
-        var_dump(file_get_contents(BASE_PATH.'/html/vf-documentation/'.$url));
-        $this->_helper->viewRenderer->setNoRender(true);
+
+        $nav = file_get_contents(BASE_PATH.'/html/vf-documentation/toc.htm');
+        
+        $nav = new Zend_Dom_Query($nav);
+        $nav = $nav->query('body');
+
+        foreach($nav as $match) {
+            $nav = $match->ownerDocument->saveXML($match);
+        }
+
+        $nav = preg_replace('/<a href="/', '<a href="/documentation/index/', $nav);
+
+        $file = BASE_PATH.'/html/vf-documentation/'.$url;
+        if(file_exists($file)) {
+            $body = file_get_contents($file);
+            $body = new Zend_Dom_Query($body);
+            $body = $body->query('body');
+
+            foreach($body as $match) {
+                $body = $match->ownerDocument->saveXML($match);
+            }
+        } else {
+            $body = $file;
+        }
+        $this->view->body = $body;
+        $this->view->nav = $nav;
     }
 
 }

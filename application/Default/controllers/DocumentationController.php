560fabcd431dddc6e789ee2e39089ef0992a8328
josh <josh.ribakoff@gmail.com>
2013-10-27 17:29:59 -0400

    Made all docs accessible, redirect old url to vfmagento3 by default

--- application/Default/controllers/DocumentationController.php
+++ application/Default/controllers/DocumentationController.php
@@ -1,14 +1,37 @@
 <?php
 class DocumentationController extends Zend_Controller_Action
 {
-
     function indexAction()
     {
+        header('Location:/documentation/vfmagento3');
+        exit;
+    }
+
+    function vfmagento3Action()
+    {
+        $this->showdocs('vfmagento-3.x');
+        $this->render('index');
+    }
+
+    function vfmagento2Action()
+    {
+        $this->showdocs('vfmagento-2.x');
+        $this->render('index');
+    }
+
+    function vfprestashop1Action()
+    {
+        $this->showdocs('vfprestashop-1.x');
+        $this->render('index');
+    }
+
+    function showdocs($version)
+    {
         $this->view->headTitle('Vehicle Fits Documentation');
-        $url = str_replace('/documentation/index/','',$this->_request->getRequestUri());
+        $url = preg_replace('#\/documentation\/(.*)\/#','',$this->_request->getRequestUri());
         $url = str_replace('..', '', $url);
 
-        $nav = file_get_contents(BASE_PATH.'/application/Default/views/scripts/vf-documentation/v3/toc.htm');
+        $nav = file_get_contents(BASE_PATH.'/application/Default/views/scripts/vf-documentation/'.$version.'/toc.htm');
 
         $nav = new Zend_Dom_Query($nav);
         $nav = $nav->query('body');
@@ -17,10 +40,22 @@ class DocumentationController extends Zend_Controller_Action
             $nav = $match->ownerDocument->saveXML($match);
         }
 
-        $nav = preg_replace('/<a href="/', '<a href="/documentation/index/', $nav);
+        switch($version) {
+            case 'vfprestashop-1.x':
+                $nav = preg_replace('/<a href="/', '<a href="/documentation/vfprestashop1/', $nav);
+                $file = BASE_PATH.'/application/Default/views/scripts/vf-documentation/vfprestashop-1.x/'.$url;
+            break;
+            case 'vfmagento-2.x':
+                $nav = preg_replace('/<a href="/', '<a href="/documentation/vfmagento2/', $nav);
+                $file = BASE_PATH.'/application/Default/views/scripts/vf-documentation/vfmagento-2.x/'.$url;
+            break;
+            case 'vfmagento-3.x':
+                $nav = preg_replace('/<a href="/', '<a href="/documentation/vfmagento3/', $nav);
+                $file = BASE_PATH.'/application/Default/views/scripts/vf-documentation/vfmagento-3.x/'.$url;
+            break;
+        }
         $nav = preg_replace('/<\/?body>/', '', $nav);
 
-        $file = BASE_PATH.'/application/Default/views/scripts/vf-documentation/v3//'.$url;
         if(file_exists($file)) {
             $body = file_get_contents($file);
             $body = new Zend_Dom_Query($body);

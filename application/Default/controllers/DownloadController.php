180299b3023bc7d7dd4d411d07642817ba9a3c64
josh <josh.ribakoff@gmail.com>
2013-10-27 16:48:22 -0400

    Added download pages for prestashop/magento separately

--- application/Default/controllers/DownloadController.php
+++ application/Default/controllers/DownloadController.php
@@ -4,9 +4,22 @@ class DownloadController extends Zend_Controller_Action
 
     function indexAction()
     {
-        $this->view->headTitle('Download Vehicle Fits');
+        header('Location:/download/magento');
+        exit;
+    }
+
+    function magentoAction()
+    {
+        $this->view->headTitle('Download Vehicle Fits for Magento');
         $this->render('navigation','navigation',true);
-        $this->render('download','default',true);
+        $this->render('download-magento','default',true);
+    }
+
+    function prestashopAction()
+    {
+        $this->view->headTitle('Download Vehicle Fits for Prestashop');
+        $this->render('navigation','navigation',true);
+        $this->render('download-prestashop','default',true);
     }
 
 }
